d891884022d7383e5d232a6d463720db3874b4f6
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-27 17:35:59 +0100

    changed product card

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -11,7 +11,7 @@ const Product = ({ product }) => {
       <img className='product__image' src={image} alt={name}></img>
       <h1 className='product__name'>{name}</h1>
       <div className='product__info'>
-        <p>{brand}</p>
+
             <Link key={product.id} to={`/product/info/${product.id}`}>
               More Info
             </Link>
